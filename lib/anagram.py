ea3770adbe9f7c1d33703c2b9c26301c2a10223a
Ahmed yussuf Abdullahi <ahmedyussuf917@gmail.com>
2023-11-29 11:23:32 +0300

    completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(this, term):
+        this.term = sorted(term.lower())
+
+    def match(this, word_list):
+        return [w for w in word_list if this._is_anagram(w)]
+
+    def _is_anagram(this, other_word):
+        return this.term == sorted(other_word.lower())
+
+
+
